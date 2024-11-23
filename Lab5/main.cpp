5bac82469f59f2fe0b92f07b4547d38712f2bb39
Rissochek <ruslanynysov@mail.ru>
2024-11-23 11:11:57 +0300

    sth

--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -11,7 +11,6 @@ struct Person {
     Person(const std::string& name, int age, const std::string& address)
         : name(name), age(age), address(address) {}
 
-    // Перегрузка оператора вывода
     friend std::ostream& operator<<(std::ostream& os, const Person& person) {
         os << "Name: " << person.name << ", Age: " << person.age << ", Address: " << person.address;
         return os;
@@ -46,22 +45,18 @@ int main() {
     std::pmr::polymorphic_allocator<int> allocator_new(&custom_alloc_new);
     DoublyLinkedList<Person> list_new(allocator_new);
 
-    // Добавляем несколько объектов в список
     list_new.push_back(Person("Alice", 30, "1234 Elm Street"));
     list_new.push_back(Person("Bob", 25, "5678 Oak Avenue"));
     list_new.push_back(Person("Charlie", 35, "91011 Pine Road"));
 
-    // Выводим все объекты из списка
     std::cout << "List of persons after push_back operations:" << std::endl;
     for (const auto& person : list_new) {
         std::cout << person << std::endl;
     }
 
-    // Удаляем элементы с начала и конца
     list_new.pop_front();
     list_new.pop_back();
 
-    // Выводим список после удаления элементов
     std::cout << "\nList of persons after pop_front and pop_back:" << std::endl;
     for (const auto& person : list_new) {
         std::cout << person << std::endl;
